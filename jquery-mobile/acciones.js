077454109ad9953d15259f1801974db8da405946
alumno365ecb16 <maritza_trejo36@gmail.com>
2015-08-20 09:18:15 -0500

    subidauno
    No me repruebe

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,68 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
+		$('#ingredientes').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+});
+
+$('#ingredientes2').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+});
+      $('#ingredientes3').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  
+	  $('#ingredientes4').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#ingredientes5').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#ingredientes6').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#ingredientes7').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#ingredientes8').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#ingredientes9').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#progreso').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  	  $('#taran').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  $('#pucha').on ("onclick", function(){
+			navigator.notification.beep(1);
+			navigator.notification.vibrate(100);
+			
+	  });
+	  	},false);//ready device
+});//document
